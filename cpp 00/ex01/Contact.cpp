b84960dfd7af70a2d18b0d15d3916e07a33e34ef
idleira home <beliaieva.iryna@gmail.com>
2024-09-17 21:37:53 +0200

    improving readability

--- cpp 00/ex01/Contact.cpp
+++ cpp 00/ex01/Contact.cpp
@@ -1,53 +1,26 @@
 #include "Contact.hpp"
 
+//constructor to initialize Contact object to empty strings
 Contact::Contact(): firstName(""), lastName(""), nickname(""), phoneNumber(""), darkestSecret("") {}
 
-std::string Contact::getFirstName(void)
-{
-	return firstName;
-}
+//return the value of the private variables (read-only)
+std::string Contact::getFirstName(void) { return firstName; }
 
-std::string Contact::getLastName(void)
-{
-	return lastName;
-}
+std::string Contact::getLastName(void) { return lastName; }
 
-std::string Contact::getNickname(void)
-{
-	return nickname;
-}
+std::string Contact::getNickname(void) { return nickname; }
 
-std::string Contact::getPhoneNumber(void)
-{
-	return phoneNumber;
-}
+std::string Contact::getPhoneNumber(void) { return phoneNumber; }
 
-std::string Contact::getDarkestSecret(void)
-{
-	return darkestSecret;
-}
+std::string Contact::getDarkestSecret(void) { return darkestSecret; }
 
-void Contact:: setFirstName(std::string new_firstName)
-{
-	firstName = new_firstName;
-}
+//set the value of the private variables (modify)
+void Contact:: setFirstName(std::string new_firstName) { firstName = new_firstName; }
 
-void Contact:: setLastName(std::string new_lastName)
-{
-	lastName = new_lastName;
-}
+void Contact:: setLastName(std::string new_lastName) { lastName = new_lastName; }
 
-void Contact:: setNickname(std::string new_nickname)
-{
-	nickname = new_nickname;
-}
+void Contact:: setNickname(std::string new_nickname) { nickname = new_nickname; }
 
-void Contact:: setPhoneNumber(std::string new_phoneNumber)
-{
-	phoneNumber = new_phoneNumber;
-}	
+void Contact:: setPhoneNumber(std::string new_phoneNumber) { phoneNumber = new_phoneNumber; }
 
-void Contact:: setDarkestSecret(std::string new_darkestSecret)
-{
-	darkestSecret = new_darkestSecret;
-}
+void Contact:: setDarkestSecret(std::string new_darkestSecret) { darkestSecret = new_darkestSecret; }
