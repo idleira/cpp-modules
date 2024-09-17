a2fd087994da8cc42db220d71a8c8f49b34a4a4d
idleira home <beliaieva.iryna@gmail.com>
2024-09-17 21:38:07 +0200

    added comments

--- cpp 00/ex01/Contact.hpp
+++ cpp 00/ex01/Contact.hpp
@@ -5,7 +5,7 @@
 # include <iostream>
 # include <string>
 
-class Contact
+class Contact							// represents a single contact
 {
 	private:
 		std::string firstName;
@@ -15,14 +15,16 @@ class Contact
 		std::string darkestSecret;
 
 	public:
-		Contact(void);
+		Contact(void);								// constructor to initialize Contact object
 
+//return the value of the private variables
 		std::string getFirstName(void);
 		std::string getLastName(void);
 		std::string getNickname(void);
 		std::string getPhoneNumber(void);
 		std::string getDarkestSecret(void);
 
+//set the value of the private variables
 		void setFirstName(std::string new_firstName);
 		void setLastName(std::string new_lastName);
 		void setNickname(std::string new_nickname);
