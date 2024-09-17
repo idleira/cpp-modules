05dac7f7d4490d2900d34c9dac0bbbcfe7c93a5b
ibeliaie <ibeliaie@student.42wolfsburg.de>
2024-09-17 18:58:26 +0200

    contact creation messages

---
+++ cpp 00/ex01/PhoneBook.cpp
@@ -0,0 +1,16 @@
+#include "PhoneBook.hpp"
+
+PhoneBook::PhoneBook(void) {
+	contactIndex = 0; }
+
+void PhoneBook::addContact(void)
+{
+	Contact newContact;
+
+	std::cout << "time to make a new contact! ꒰ᐢ. .ᐢ꒱ 📚📞" << std::endl;
+	std::cout << "just fill in the details below to get started:\n" << std::endl;
+
+	std::cout << "first name: ";
+
+	
+}
