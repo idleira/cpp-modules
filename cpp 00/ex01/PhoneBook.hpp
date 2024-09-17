247c8c7f987e2bb35513294600ce0608a0ba054b
idleira home <beliaieva.iryna@gmail.com>
2024-09-17 21:39:17 +0200

    wip

--- cpp 00/ex01/PhoneBook.hpp
+++ cpp 00/ex01/PhoneBook.hpp
@@ -3,22 +3,28 @@
 # define PHONEBOOK_HPP
 
 # include "Contact.hpp"
+# include <iomanip>
+# include <iostream>
+# include <string>
+# include <cstdlib>
+# include <cstdio>
 
-class PhoneBook
+class Contact;
+class PhoneBook						// manages collection of contacts
 {
 	private:
-		Contact contacts[8];
-		int		contactIndex;
+		Contact contacts[8];		// array of 8 contacts
+		int		contactIndex;		// index of the contact to be added
 
 	public:
-		PhoneBook(void);
+		PhoneBook(void);			// constructor to initialize PhoneBook object
 
 		void addContact(void);
 		void searchContact(void);
-		void printContact(void);
-		void printPhoneBook(void);
+		// void printContact(void);
+		// void printPhoneBook(void);
 		
-		Contact getContact(int contactIndex);
+		Contact getContact(int contactIndex);		// function to get a Contact from contacts array
 };
 
 
