d600423ac94220c5be71198bc5b5ea78cbf69680
idleira home <beliaieva.iryna@gmail.com>
2024-09-17 21:39:06 +0200

    taking user input

--- cpp 00/ex01/PhoneBook.cpp
+++ cpp 00/ex01/PhoneBook.cpp
@@ -1,16 +1,61 @@
 #include "PhoneBook.hpp"
 
-PhoneBook::PhoneBook(void) {
-	contactIndex = 0; }
+PhoneBook::PhoneBook(void) { contactIndex = 0; }
 
 void PhoneBook::addContact(void)
 {
 	Contact newContact;
+	std::string input;
 
-	std::cout << "time to make a new contact! ꒰ᐢ. .ᐢ꒱ 📚📞" << std::endl;
+	std::cout << "time to make a new contact! 📚📞" << std::endl;
 	std::cout << "just fill in the details below to get started:\n" << std::endl;
 
-	std::cout << "first name: ";
+	std::cout << "first name:\n";
+	std::cin >> input;
+	newContact.setFirstName(input);
+	std::cout << "last name:\n";
+	std::cin >> input;
+	newContact.setLastName(input);
+	std::cout << "nickname:\n";
+	std::cin >> input;
+	newContact.setNickname(input);
+	std::cout << "phone number:\n";
+	std::cin >> input;
+	newContact.setPhoneNumber(input);
+	std::cout << "darkest secret:\n";
+	std::cin >> input;
+	newContact.setDarkestSecret(input);
 
-	
+	contacts[contactIndex %8] = newContact;
+	contactIndex++;
+
+	std::cout << "contact added! 👤✅\n " << std::endl;
+}
+
+void PhoneBook::searchContact(void)
+{
+	std::string input;
+	int	index = 0;
+
+	if (!index)
+	{
+		std::cout << "ADD a contact before SEARCHing" << std::endl;
+		return ;
+	}
+	// printPhoneBook();
+	std::cout << "searching for a contact! 🔍📞" << std::endl;
+	std::cout << "please enter the index of the contact you would like to view:\n" << std::endl;
+	std::cin >> input;
+	index = std::stoi(input);
+
+	if (index >= 0 && index < contactIndex)
+	{
+		std::cout << "first name: " << contacts[index].getFirstName() << std::endl;
+		std::cout << "last name: " << contacts[index].getLastName() << std::endl;
+		std::cout << "nickname: " << contacts[index].getNickname() << std::endl;
+		std::cout << "phone number: " << contacts[index].getPhoneNumber() << std::endl;
+		std::cout << "darkest secret: " << contacts[index].getDarkestSecret() << std::endl;
+	}
+	else
+	{ std::cout << "contact not found! 🚫📞" << std::endl; }
 }
